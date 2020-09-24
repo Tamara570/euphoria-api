0df5ce93825ae3b8da3d5a37ee2fd39827a82058
Tamara Walker <tamaralw115@gmail.com>
2020-09-24 07:00:29 -0400

    updated CORS

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const { CLIENT_ORIGIN } = require('./config')
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -13,7 +13,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+)
 app.use(express.json())
 
 app.get('/', (req, res) => {
