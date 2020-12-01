6e391c2273fc5ba06ba895e5fd1aca0409bab12b
Tamara Walker <tamaralw115@gmail.com>
2020-12-01 09:10:33 -0500

    updated file to get ready to be deployed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/euphoria',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://xyfoxqojvyzcrv:5a07f1cdf99dc1971b9fb563ef51144c2ea970ef0338646d8bf211d1181fc1a2@ec2-34-237-236-32.compute-1.amazonaws.com:5432/d8ttk2trn0tvp4',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/euphoria-test',
     CLIENT_ORIGIN: '*'
   }
--- postgrator-config.js
+++ postgrator-config.js
@@ -7,4 +7,9 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+  "host": process.env.MIGRATION_DATABASE_HOST,
+  "port": process.env.MIGRATION_DATABASE_PORT,
+  "database": process.env.MIGRATION_DATABASE_NAME,
+  "username": process.env.MIGRATION_DATABASE_USER,
+  "password": process.env.MIGRATION_DATABASE_PASS
 }
